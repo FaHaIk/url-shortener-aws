8892b0498c31ecbec2a5ea2cf80af6ae50898bad
FaHaIk <fabian.ikizoglu@web.de>
2021-08-25 13:30:41 +0200

    functions chained

--- src/App.js
+++ src/App.js
@@ -18,16 +18,20 @@ function App() {
   }
 
   const Enter = e => {
-    callAPI("Peter", "Zornig")
+    callAPI()
   }
 
-  const callAPI = (firstName, lastName) => {
+  const callAPI = () => {
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var raw = JSON.stringify({ "firstName": firstName, "lastName": lastName });
+    var raw = JSON.stringify({
+      "isPublic": "0",
+      "expirationDate": "12.06.2022",
+      "longLink": "https://www.derda.net"
+    });
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
       method: 'POST',
@@ -36,7 +40,7 @@ function App() {
       redirect: 'follow'
     };
     // make API call with parameters and use promises to get response
-    fetch("https://aq7cww6x21.execute-api.eu-central-1.amazonaws.com/dev", requestOptions)
+    fetch("https://ctqcpzxkd1.execute-api.eu-central-1.amazonaws.com/dev", requestOptions)
       .then(response => response.text())
       .then(result => alert(JSON.parse(result).body))
       .catch(error => console.log('error', error));
