51258ff612267c01efefd6b739c5c9b75e34d8c7
FaHaIk <fabian.ikizoglu@web.de>
2021-08-25 16:10:10 +0200

    add fe functionality

--- src/App.js
+++ src/App.js
@@ -4,44 +4,52 @@ import 'antd/dist/antd.css';
 import { Input, Radio, DatePicker, Checkbox } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
-const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "any-default-local-build_env"; 
+// const BUILD_ENV = process.env.REACT_APP_BUILD_ENV || "any-default-local-build_env";
 
 function App() {
-  const [visibility, setVisibility] = React.useState(1);
+  const [isPublic, setIsPublic] = React.useState(1);
+  const [expirationDate, setExpirationDate] = React.useState("3000-10-10");
+  const [link, setLink] = React.useState("");
   const [isDatepickerDisabled, setIsDatepickerDisabled] = React.useState(true);
 
-  const onChangeVisibility = e => {
-    console.log('radio checked', e.target.value);
-    setVisibility(e.target.value);
+  const onChangePublic = e => {
+    setIsPublic(e.target.value);
   };
 
   const onChangeHasExpirationDate = e => {
     setIsDatepickerDisabled(e.target.checked)
   }
 
-  const Enter = e => {
+  const onChange = (e) => {
+    setLink(e.target.value)
+  }
+
+  const onPressEnter = (e) => {
     callAPI()
   }
 
+  const onChangeDatePicker = (date, dateString) => {
+    setExpirationDate(dateString)
+  }
+
   const callAPI = () => {
-    // instantiate a headers object
+
     var myHeaders = new Headers();
-    // add content type header to object
     myHeaders.append("Content-Type", "application/json");
-    // using built in JSON utility package turn object to string and store in a variable
+
     var raw = JSON.stringify({
-      "isPublic": "1",
-      "expirationDate": "12.06.000",
-      "longLink": "https://www.barnicles.net"
+      "isPublic": isPublic,
+      "expirationDate": expirationDate,
+      "longLink": link
     });
-    // create a JSON object with parameters for API call and store in a variable
+
     var requestOptions = {
       method: 'POST',
       headers: myHeaders,
       body: raw,
       redirect: 'follow'
-    };
-    // make API call with parameters and use promises to get response
+    }
+
     fetch(process.env.REACT_APP_DB, requestOptions)
       .then(response => response.text())
       .then(result => alert(JSON.parse(result).body))
@@ -51,28 +59,30 @@ function App() {
   return (
     <div className="App">
       <div className="center-wrapper">
-        <Input onPressEnter={Enter} size="large" placeholder="Paste long URL and shorten it!" prefix={<SearchOutlined />} />
-        <div className="spacer"></div>
-        <div className="settings-wrapper">
-          <div style={{ alignSelf: "center" }}>
-            <Radio.Group onChange={onChangeVisibility} value={visibility}>
-              <Radio value={1}>Public</Radio>
-              <Radio value={2}>Private</Radio>
-            </Radio.Group>
+        <form id="myForm" onSubmit={onPressEnter}>
+          <Input onChange={onChange} onPressEnter={onPressEnter} size="large" placeholder="Paste long URL and shorten it!" prefix={<SearchOutlined />} />
+          <div className="spacer"></div>
+          <div className="settings-wrapper">
+            <div style={{ alignSelf: "center" }}>
+              <Radio.Group onChange={onChangePublic} value={isPublic}>
+                <Radio value={1}>Public</Radio>
+                <Radio value={0}>Private</Radio>
+              </Radio.Group>
+            </div>
+            <div>
+              <Checkbox defaultChecked onChange={onChangeHasExpirationDate}>Never Expire</Checkbox>
+              <DatePicker
+                onChange={onChangeDatePicker}
+                disabled={isDatepickerDisabled}
+                placeholder="Expiration date"
+                disabledDate={(current) => {
+                  return current.valueOf() < Date.now();
+                }}
+                bordered={false}
+              />
+            </div>
           </div>
-          <div>
-            <Checkbox defaultChecked onChange={onChangeHasExpirationDate}>Never Expire</Checkbox>
-            <DatePicker
-              disabled={isDatepickerDisabled}
-              placeholder="Expiration date"
-              disabledDate={(current) => {
-                return current.valueOf() < Date.now();
-              }}
-              bordered={false}
-            />
-          </div>
-        </div>
-
+        </form>
       </div>
     </div>
   );
